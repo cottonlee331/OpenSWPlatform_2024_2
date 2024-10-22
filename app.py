0bef8fb5348145b3995e0fb5c35237557f41ac3f
SominLee <shl_4869@ewhain.net>
2024-10-22 21:57:55 +0900

    Feat: html 구현

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ application = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@application.route("/season")
+def view_list():
+    return render_template("season.html")
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=Ture)
